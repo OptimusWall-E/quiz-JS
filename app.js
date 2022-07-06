c66b8270683240137257429f1ab6574d713b899f
OptimusWall-E <iemailedmichael@gmail.com>
2022-07-06 08:09:27 +0100

    added form to index.html and to Express in app.js

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <form action="/info/get" method="GET">
+      <input type="submit" value="GET">
+    </form>
+  </body>
+  </html>`)
 })
 
 app.listen(port, () => {
